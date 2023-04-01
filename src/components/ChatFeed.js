66ac71464dec57ea1c7842ed1e8ec871992c000b
Michelle Chen <chensiqi96@gmail.com>
2023-04-01 11:17:32 -0700

    modified chatfeed

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -18,10 +18,12 @@ const ChatFeed = (props) => {
       //if there are messages we need to find the last nessage
       const isMyMessage = userName === message.sender.username//check the current user's username against the sender of this message's username
 
+      //we need to return th message component and check whether the messages are ours vs others to apply specific styling
       return (
         <div key={`msg_${index}`} style={{ width: '100%' }}>
           <div className="message-block">
             {isMyMessage
+            //need to pass message accessed as specific key as a meesage to the message components
               ? <MyMessage message={message} />
               : <OtherMessage message={message}/>}
           </div>
@@ -30,12 +32,14 @@ const ChatFeed = (props) => {
     });
   };
 
-  if (!chat) return <div />;
+  //need to create a way to notify use when chat is still loading 
+  if (!chat) return 'Chat is loading please wait...';
 
   return (
     <div className="chat-feed">
-      <ChatTitle chatTitle= {chat?.title}/>
+      <ChatTitle chatTitle= {chat.title}/>
       {renderMessages()}
+      {/* leave some space off so the messages don't overlap the form */}
       <div style={{ height: '100px' }} />
       <MessageForm {...props} chatId={activeChat} />
     </div>
